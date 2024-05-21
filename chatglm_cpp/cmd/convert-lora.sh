a7b5c34463705f321ae38f2f144cb5746e1592b1
liusanlu <sanlu.liu@learnable.ai>
2024-05-21 15:20:40 +0800

    feat:update

---
+++ chatglm_cpp/cmd/convert-lora.sh
@@ -0,0 +1,1 @@
+python3 chatglm_cpp/convert.py -i /Users/ragin/.cache/modelscope/hub/ZhipuAI/chatglm3-6b -t q4_0 -o models/chatglm-ggml.bin  -l /Users/ragin/codes/fe/personal/LLM/chatglm-cpp/chatglm_cpp/lora-points
